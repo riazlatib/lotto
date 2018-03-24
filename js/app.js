1f3f5a032bfa3b1105151bdf20accd1e5360188f
Riaz Latib <riazlatib@gmail.com>
2018-03-23 23:34:41 -0400

    Foundation

--- js/app.js
+++ js/app.js
@@ -1,18 +1,33 @@
+// ----------------------------------------------- Ready Stage
 jQuery(document).ready(function() {
 
-  var set_one = '11';
-  var set_two = '22';
-  var set_three = '33';
-  var set_four = '44';
-  var set_five = '55';
-  var set_six = '66';
+  // Init setup
+  lotto_gen();
 
-  var numbers = set_one + set_two + set_three + set_four + set_five + set_six;
+  // On click event
+  jQuery('#button').on('click', function() {
+    lotto_gen();
+  });
+});
+// ----------------------------------------------- Ready Stage
+
+// ----------------------------------------------- Lotto number generator
+function lotto_gen() {
+  var set_one = Math.floor(Math.random() * 53);
+  var set_two = Math.floor(Math.random() * 53);
+  var set_three = Math.floor(Math.random() * 53);
+  var set_four = Math.floor(Math.random() * 53);
+  var set_five = Math.floor(Math.random() * 53);
+  var set_six = Math.floor(Math.random() * 53);
+
+  var numbers = set_one.toString() + set_two.toString() + set_three.toString() + set_four.toString().toString() + set_five.toString() + set_six.toString();
 
   jQuery('.lotto_numbers').val(numbers);
 
+  // Number format mask
   var cleave = new Cleave('.lotto_numbers', {
     blocks: [2,2,2,2,2,2],
     uppercase: true
   });
-});
+}
+// ----------------------------------------------- Lotto number generator
