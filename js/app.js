2681f2c47bcd62e65f994221f8bda50358078cdb
Riaz Latib <riazlatib@gmail.com>
2018-03-23 22:27:07 -0400

    Init layout

--- js/app.js
+++ js/app.js
@@ -1,45 +1,3 @@
-var NumberOfWords = 28
-var words = new BuildArray(NumberOfWords)
-
-// Use the following variables to
-// define your random words:
-words[1] = "escapology"
-words[2] = "brightwork"
-words[3] = "verkrampte"
-words[4] = "protectrix"
-words[5] = "nudibranch"
-words[6] = "grandchild"
-words[7] = "newfangled"
-words[8] = "flugelhorn"
-words[9] = "mythologer"
-words[10] = "pluperfect"
-words[11] = "jellygraph"
-words[12] = "quickthorn"
-words[13] = "rottweiler"
-words[14] = "technician"
-words[15] = "cowpuncher"
-words[16] = "middlebrow"
-words[17] = "jackhammer"
-words[18] = "triphthong"
-words[19] = "wunderkind"
-words[20] = "dazzlement"
-words[21] = "jabberwock"
-words[22] = "witchcraft"
-words[23] = "pawnbroker"
-words[24] = "thumbprint"
-words[25] = "motorcycle"
-words[26] = "cryptogram"
-words[27] = "torchlight"
-words[28] = "bankruptcy"
-
-function BuildArray(size){
-  this.length = size
-  for (var i = 1; i <= size; i++){
-    this[i] = null
-  }
-  return this
-}
-
 function lotto() {
 // ---------------------------------------------------------------------------- ROUGH DRAFT
 // BASIC SENTENCE STRUCTURE
@@ -69,5 +27,4 @@ var sentance = subject[subject_random] + ' ' + predicate[predicate_random] + ' '
 
 document.getElementById("wordbox").innerHTML = sentance;
 // ---------------------------------------------------------------------------- ROUGH DRAFT
-
 }
